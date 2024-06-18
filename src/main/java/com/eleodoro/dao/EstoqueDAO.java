ba4cdf92ec43d6881dba0e78e39d682ffb9b12bb
victorhsg01 <victor.schroeder.goncalves@escola.pr.gov.br>
2024-06-18 12:15:07 -0300

    dao comecando

---
+++ src/main/java/com/eleodoro/dao/EstoqueDAO.java
@@ -0,0 +1,35 @@
+package com.eleodoro.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.eleodoro.conexao.Conexao;
+
+public class EstoqueDAO {
+
+    public void Estoque(EstoquePOJO estoquePojo){
+
+        Conexao conexao = new Conexao();
+
+        String sql = "insert into estoque (capacidade) values (?)";
+
+        PreparedStatement ps = null;
+
+        try {
+            ps = conexao.getConexao().prepareStatement(sql);
+            
+            ps.setString(1, estoquePojo.getCapacidade());
+            
+            ps.execute();
+            ps.close();
+
+            System.out.println("Dados gravadas com sucesso");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Houve um problema no EstoqueDAO");
+        }
+    
+    }
+    
+}
